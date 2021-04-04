efe0627cc67cd6541fece440f70f899c049954ac
Masrur Sadiev <masrur.sadiev@outlook.com>
2021-04-04 09:55:05 -0400

    Showes distance for each hour.

---
+++ DistanceCalculator/DistanceCalculator/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace DistanceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int distance;
+            bool exit = true;
+
+            Console.WriteLine("Welcome to DistanceCalculator");
+
+           
+            
+
+
+            while (exit)
+            {
+                
+                Console.WriteLine("\nMENU");
+                Console.WriteLine("1.Start program");
+                Console.WriteLine("2.Exit");
+                Console.WriteLine("Please enter a number from the menu");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Console.WriteLine("Please enter how long you will drive");
+                        int hours = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Please enter your speed");
+                        int speed = Convert.ToInt32(Console.ReadLine());
+                        int counter = 1;
+                        while (counter <= hours)
+                        {
+                            // Console.WriteLine(counter);
+
+                            distance = counter * speed;
+                            Console.WriteLine($"After {counter} hour the distance is {distance} miles");
+                            counter++;
+                        }
+                        break;
+                    case "2":
+                        exit = false;
+                        break;
+                    default:
+                        Console.WriteLine("Error, please enter a number from the menu");
+                        break;
+                }
+            }
+            
+        }
+    }
+}
